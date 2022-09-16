1b6e656a2ac3574d27322b9a2e4e8e7c2e6a4ab4
norstein21 <habieputra3@gmail.com>
2022-09-16 23:04:23 +0700

    First push with backend code

---
+++ backend/routes/UserRoute.js
@@ -0,0 +1,18 @@
+import express, { Router } from "express";
+import {
+    getUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser
+} from "../controllers/UserController";
+
+const router = express.Router();
+
+router.get('/users',getUsers);
+router.get('/users/:id',getUserById);
+router.post('/users',createUser);
+router.patch('users/:id',updateUser);
+router.delete('/users/:id',deleteUser);
+
+export default router;
